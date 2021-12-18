8f14aaafb14a12fe8bb51f0408f225541e42470e
VuQuangHuy-0412 <vqhuybkhn@gmail.com>
2021-12-19 00:08:22 +0700

    connect database

--- src/main/java/EcoBikeRental/Controller/HomeController.java
+++ src/main/java/EcoBikeRental/Controller/HomeController.java
@@ -1,14 +1,28 @@
 package EcoBikeRental.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import EcoBikeRental.Dao.HomeDao;
 
 @Controller
 public class HomeController {
+	@Autowired
+	HomeDao homeDao;
 
 	@RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
-	public String index() {
-		return "index";
+	public ModelAndView index() {
+		ModelAndView mav = new ModelAndView("index");
+		mav.addObject("users", homeDao.getAllUser());
+		return mav;
+	}
+	
+	@RequestMapping(value = "/product", method = RequestMethod.GET)
+	public ModelAndView product() {
+		ModelAndView mav = new ModelAndView("index");
+		return mav;
 	}
 }
---
+++ src/main/java/EcoBikeRental/Entity/User.java
@@ -0,0 +1,72 @@
+package EcoBikeRental.Entity;
+
+public class User {
+	private Integer userId;
+	private String fullName;
+	private String email;
+	private String phone;
+	private Integer status;
+	private String createdAt;
+	private String updatedAt;
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public String getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(String updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+}
